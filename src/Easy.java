46a94dba6eecaf46065dcd2128f4c45ab84c890f
sifatul <sifii2013@gmail.com>
2020-08-22 15:12:51 +0900

    solved reverse

--- src/Easy.java
+++ src/Easy.java
@@ -3,14 +3,15 @@ public class Easy {
 
         int isNegative = x<0?-1:1;
         x *= isNegative;
-        int output =0;
+        double output =0;
         while(x>0){
             int dividend = x/10;
             int remainder = x%10;
             output = output*10+remainder;
             x= dividend;
         }
+        output = output*isNegative;
 
-        return output*isNegative;
+        return output > Integer.MAX_VALUE || output<Integer.MIN_VALUE ? 0 : (int)output;
     }
 }
