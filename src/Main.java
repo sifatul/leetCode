7cd5a5a4c4965d1561d12184ae2377db6689d4e6
sifatul <sifii2013@gmail.com>
2020-08-23 14:52:40 +0900

    solved searchInsert

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,8 @@ public class Main {
 //
 
         Easy easy = new Easy();
-        int output = easy.removeElement( new int[]{0,1,2,2,3,0,4,2},2);
+        int output = easy.searchInsert( new int[]{1,3,5,6},2);
+//        int output = easy.searchInsert("aaaaa","bb");
 //        listNode.print(output);
         System.out.println(output);
     }
