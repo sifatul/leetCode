2b0df62f12cf9f49c267d7e4229faeb23d7dfde7
sifatul <sifii2013@gmail.com>
2020-09-01 21:06:11 +0900

    solved pluOne and optimized

--- src/Easy.java
+++ src/Easy.java
@@ -262,6 +262,20 @@ public class Easy {
         }
         return output;
     }
+    public int[] plusOne(int[] digits) {
+        for (int i=digits.length-1; i>=0; i--){
+            digits[i]++;
+            if(digits[i]<=9)
+                return digits;
+
+            digits[i] = 0;
+            System.out.println("current : "+digits[i]);
+        }
+        int [] output = new int[digits.length+1];
+        output[0]=1;
+
+        return output;
+    }
 }
 
 
