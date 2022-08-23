4b5c051d5807edecb76cc8323217daa50c0f6116
sifatul <sifatul@teraark.com>
2022-08-23 11:08:57 +0900

    chore : mrege two sorted list

---
+++ merge-two-sorted-lists/java/recursion.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public ListNode mergeList (ListNode list1, ListNode list2){
+         if(list1 ==null) return list2;
+         if(list2 ==null) return list1;
+        if(list1.val<list2.val){
+           
+               list1.next = mergeList(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = mergeList(list1, list2.next);
+            return list2;
+        }
+        
+        
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        return mergeList(list1, list2); 
+        
+    }
+}
